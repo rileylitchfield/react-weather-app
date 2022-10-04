c5b543a5c769a8cfcfa22452185020d64a5ea131
Riley <rileylitchfield95@gmail.com>
2022-10-03 20:00:09 -0500

    Added apiCall function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+      loading: false
+    };
+    this.apiCall = this.apiCall.bind(this);
+  }
+
+  // Fetch data from OpenWeatherAPI
+  apiCall() {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=houston&appid=1bc5008f210ae0aac20c8d13e30e378a&units=imperial`)
+      .then((response) => {
+        this.setState({
+          loading: true
+        })
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+      });
+  }
+
+  componentDidMount() {
+    this.apiCall();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={this.apiCall}>test</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
