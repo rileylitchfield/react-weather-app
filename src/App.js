c41df5bc66d780446f7c69f26adf3c7e514f5d75
Riley <rileylitchfield95@gmail.com>
2022-10-05 11:26:54 -0500

    Updated JSX

--- src/App.js
+++ src/App.js
@@ -31,10 +31,18 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <button onClick={this.apiCall}>test</button>
-        </header>
+      <div className="container-fluid bg-info vh-100 vw-100 d-flex flex-column align-items-center justify-content-center p-5">
+        <div className="row flex-grow-1">
+          <div className="col-12">
+            <div className="d-flex">
+              <input className="form-control shadow-none" placeholder="Enter a city..."></input>
+              <button className="btn btn-success shadow-none" onClick={this.apiCall}>Test</button></div>
+          </div>
+        </div>
+        <div className="row flex-grow-1">
+          <div className="col-12">
+          </div>
+        </div>
       </div>
     );
   }
